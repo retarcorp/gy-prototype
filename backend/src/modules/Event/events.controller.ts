362f119fd0c99bba77f7bf3eeff6d716d8b8db67
Denis Latushkin <info@retarcorp.by>
2023-05-10 21:22:39 +0200

    Enrollment implemented

--- backend/src/modules/Event/events.controller.ts
+++ backend/src/modules/Event/events.controller.ts
@@ -1,13 +1,13 @@
 import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
 import { EventsService } from './events.service';
-import { AdminOnly, WithAuth } from '../User/Auth.guards';
-import { EventStatus } from '@prisma/client';
+import { AdminOnly, CurrentUser, WithAuth } from '../User/Auth.guards';
+import { Event, EventStatus, Participant, Registration } from '@prisma/client';
 
 @Controller('events')
 export class EventsController {
     constructor(
         private readonly eventsService: EventsService,
-    ) {}
+    ) { }
 
     @Get('/')
     @WithAuth()
@@ -22,10 +22,10 @@ export class EventsController {
         const events = await this.eventsService.getEvents([EventStatus.UPCOMING, EventStatus.DRAFT]);
     }
 
-    @Get('/:id')
-    @WithAuth()
-    async getEvent(): Promise<any> {
-        // TODO
+    @Get('/:id/dashboard')
+    @AdminOnly()
+    async getEventDashboard(@Param('id', ParseIntPipe) eventId: number): Promise<any> {
+        return await this.eventsService.getEventDashboard(eventId);
     }
 
     @Post('/')
@@ -50,5 +50,88 @@ export class EventsController {
     async updateEvent(@Param('id', ParseIntPipe) eventId: number, @Body() eventData: any): Promise<any> {
         return await this.eventsService.updateEvent(eventId, eventData);
     }
+
+    @Post('/:id/registration')
+    @WithAuth()
+    async registerForEvent(
+        @CurrentUser('id', ParseIntPipe) userId: number,
+        @Param('id', ParseIntPipe) eventId: number
+    ): Promise<any> {
+
+        return await this.eventsService.registerOnEvent(userId, eventId);
+    }
+
+    @Delete('/:id/registration')
+    @WithAuth()
+    async unregisterFromEvent(
+        @CurrentUser('id', ParseIntPipe) userId: number,
+        @Param('id', ParseIntPipe) eventId: number
+    ): Promise<any> {
+
+        const result = await this.eventsService.deleteRegistration(userId, eventId);
+        return {
+            eventId,
+            result
+        }
+    }
+
+    @Get('/available')
+    @WithAuth()
+    async getAvailableEvents(
+        @CurrentUser('id', ParseIntPipe) userId: number
+    ): Promise<Event[]> {
+        console.log(userId);
+        return await this.eventsService.getAvailableEvents(userId);
+    }
+
+    @Get('/registered')
+    @WithAuth()
+    async getRegisteredEvents(
+        @CurrentUser('id', ParseIntPipe) userId: number
+    ): Promise<Array<Registration & {event: Event}>> {
+        return await this.eventsService.getUserRegisterations(userId);
+    }
+
+    @Get('/participated')
+    @WithAuth()
+    async getParticipatedEvents(
+        @CurrentUser('id', ParseIntPipe) userId: number
+    ): Promise<Array<Participant & {event: Event}>> {
+        return await this.eventsService.getUserParticipations(userId);
+    }
+
+    @Get('/:id/registration/validate/:userId/:pin')
+    @AdminOnly()
+    async validateRegisteration(
+        @Param('id', ParseIntPipe) eventId: number,
+        @Param('userId', ParseIntPipe) userId: number,
+        @Param('pin') pin: string
+    ): Promise<any> {
+        try {
+            await this.eventsService.validateAndFetchRegistration(eventId, userId, pin);
+            return { 
+                valid: true
+            }
+        } catch (e) {
+            return {
+                valid: false
+            }
+        }
+    }
+
+    @Post('/:id/registration/validate/:userId/:pin')
+    @AdminOnly()
+    async validateRegisterationAndParticipate(
+        @Param('id', ParseIntPipe) eventId: number,
+        @Param('userId', ParseIntPipe) userId: number,
+        @Param('pin') pin: string
+    ): Promise<any> {
+        
+        const registration = await this.eventsService.validateAndFetchRegistration(eventId, userId, pin);
+        const participant = await this.eventsService.enrollOnEvent(registration.id);
+
+        return participant;
+    }
+
 
 }  
--- backend/src/modules/Event/events.service.ts
+++ backend/src/modules/Event/events.service.ts
@@ -1,5 +1,5 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { Event, EventStatus, PrismaClient } from '@prisma/client';
+import { Event, EventStatus, Participant, PrismaClient, Registration } from '@prisma/client';
 
 @Injectable()
 export class EventsService {
@@ -60,7 +60,7 @@ export class EventsService {
             where: {
                 id: eventId,
             }
-        });        
+        });
 
         if (!event) {
             throw new HttpException('Event not found!', HttpStatus.NOT_FOUND);
@@ -72,4 +72,207 @@ export class EventsService {
             }
         })
     }
+
+    async registerOnEvent(userId: number, eventId: number): Promise<Registration> {
+        const event = await this.prismaClient.event.findUnique({
+            where: {
+                id: eventId,
+            }
+        });
+
+        if (!event) {
+            throw new HttpException('Event not found!', HttpStatus.NOT_FOUND);
+        }
+
+        if (event.status !== EventStatus.UPCOMING) {
+            throw new HttpException('Cannot register on event that is not in upcoming status', HttpStatus.FORBIDDEN);
+        }
+
+        const registrations = await this.prismaClient.registration.findMany({
+            where: {
+                eventId: eventId,
+            }
+        });
+
+        if (registrations.some(registration => registration.userId === userId)) {
+            throw new HttpException('User already registered on this event!', HttpStatus.FORBIDDEN);
+        }
+
+        if (registrations.length >= event.participantLimit) {
+            throw new HttpException('Event is full!', HttpStatus.FORBIDDEN);
+        }
+
+        return await this.prismaClient.registration.create({
+            data: {
+                userId: userId,
+                eventId: eventId,
+                enrollmentPIN: Math.floor(1000 + Math.random() * 9000).toString(),
+            }
+        });
+
+    }
+
+    async deleteRegistration(userId: number, eventId: number): Promise<boolean> {
+
+        const registrations = await this.getUserRegisterations(userId);
+        const targetRegistration = registrations.find(registration => registration.eventId === eventId);
+
+        if (!targetRegistration) {
+            throw new HttpException('User not registered on this event!', HttpStatus.NOT_FOUND);
+        }
+
+        if (!([EventStatus.UPCOMING, EventStatus.OPEN] as EventStatus[]).includes(targetRegistration.event.status)) {
+            throw new HttpException('Cannot unregister from event that is not in upcoming status', HttpStatus.FORBIDDEN);
+        }
+
+        await this.prismaClient.registration.deleteMany({
+            where: {
+                userId: userId,
+                eventId: eventId,
+            }
+        });
+
+        return true;
+    }
+
+    async getAvailableEvents(userId: number): Promise<Event[]> {
+
+        const registrations = await this.getUserRegisterations(userId);
+
+        const events = await this.prismaClient.event.findMany({
+            where: {
+                status: EventStatus.UPCOMING,
+                NOT: {
+                    id: {
+                        in: registrations.map(registration => registration.eventId),
+                    }
+                }
+            }
+        });
+
+        return events;
+
+    }
+
+    async getUserRegisterations(userId: number): Promise<Array<Registration & { event: Event }>> {
+        return (await this.prismaClient.registration.findMany({
+            where: {
+                userId: userId,
+            },
+            include: {
+                event: true,
+            }
+        })).filter(registration => ([EventStatus.UPCOMING, EventStatus.OPEN] as EventStatus[]).includes(registration.event.status));
+    }
+
+    async getUserParticipations(userId: number, statuses: EventStatus[] = [EventStatus.OPEN]): Promise<Array<Participant & { event: Event }>> {
+        return (await this.prismaClient.participant.findMany({
+            where: {
+                userId: userId,
+            },
+            include: {
+                event: true,
+            }
+        })).filter(participant => statuses.includes(participant.event.status));
+    }
+
+    async getEventDashboard(eventId: number): Promise<Event & any> {
+
+        const event = await this.prismaClient.event.findUnique({
+            where: {
+                id: eventId,
+            }
+        });
+
+        if (!event) {
+            throw new HttpException('Event not found!', HttpStatus.NOT_FOUND);
+        }
+
+        const registrations = await this.prismaClient.registration.findMany({
+            where: {
+                eventId: eventId,
+            },
+            include: {
+                user: true,
+            }
+
+        });
+
+        const participants = await this.prismaClient.participant.findMany({
+            where: {
+                eventId: eventId,
+            },
+            include: {
+                user: true,
+            }
+        });
+
+        return {
+            ...event,
+            registrations: registrations,
+            participants: participants,
+        }
+    }
+
+    async validateAndFetchRegistration(userId: number, eventId: number, pin: string) {
+
+        const registration = await this.prismaClient.registration.findFirst({
+            where: {
+                userId: userId,
+                eventId: eventId,
+            }
+        });
+
+        if (!registration) {
+            throw new HttpException('Registration not found!', HttpStatus.NOT_FOUND);
+        }
+
+        if (registration.enrollmentPIN !== pin) {
+            throw new HttpException('Invalid PIN!', HttpStatus.FORBIDDEN);
+        }
+
+        return registration;
+    }
+
+    async enrollOnEvent(registrationId: number): Promise<Participant> {
+
+        const registration = await this.prismaClient.registration.findUnique({
+            where: {
+                id: registrationId,
+            },
+            include: {
+                event: true,
+            }
+        });
+
+        if (!registration) {
+            throw new HttpException('Registration not found!', HttpStatus.NOT_FOUND);
+        }
+
+        if (registration.event.status !== EventStatus.OPEN) {
+            throw new HttpException('Cannot enroll on event that is not in open status', HttpStatus.FORBIDDEN);
+        }
+
+        const participants = await this.prismaClient.participant.findMany({
+            where: {
+                eventId: registration.eventId,
+            }
+        });
+
+        if (participants.some(participant => participant.userId === registration.userId)) {
+            throw new HttpException('User already enrolled on this event!', HttpStatus.FORBIDDEN);
+        }
+
+        const participant = await this.prismaClient.participant.create({
+            data: {
+                userId: registration.userId,
+                eventId: registration.eventId,
+            }
+        });
+
+        await this.deleteRegistration(registration.userId, registration.eventId);
+
+        return participant;
+    }
+
 }
